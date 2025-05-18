d3a9e70b8d6e22d5d654631a6092a15288505604
Leif Etter <etter.leif@gmail.com>
2025-05-18 23:21:56 +0200

    change response type error to correct type

--- src/types/api/responses.ts
+++ src/types/api/responses.ts
@@ -1,7 +1,9 @@
+import { ApiError } from "../errors/ApiError";
+
 export type ApiResponse<T> = {
   success: boolean;
   data?: T;
-  error?: string;
+  error?: ApiError;
 };
 
 export type PaginatedResponse<T> = ApiResponse<{
