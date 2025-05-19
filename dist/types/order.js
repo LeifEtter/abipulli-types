4f640d864f033db92a2c6af1ef4dc2c6047999e6
Leif Etter <etter.leif@gmail.com>
2025-05-19 14:02:00 +0200

    change tsconfig emitDeclaration setting and delete and rebuild dist

--- dist/types/order.js
+++
@@ -1,8 +0,0 @@
-export var OrderStatus;
-(function (OrderStatus) {
-    OrderStatus["CREATED"] = "CREATED";
-    OrderStatus["BASIC_INFO"] = "BASIC_INFO";
-    OrderStatus["DESIGNING"] = "DESIGNING";
-    OrderStatus["REVISING"] = "REVISING";
-    OrderStatus["FINISHING_INFO"] = "FINISHING_INFO";
-})(OrderStatus || (OrderStatus = {}));
