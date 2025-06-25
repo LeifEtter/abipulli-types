44f8d08069f8f3efb4706b5dc1bc2c7ab62d8515
Leif Etter <etter.leif@gmail.com>
2025-06-25 13:24:20 +0200

    add ideogram schemas for typing ideogram requests and responses

---
+++ src/types/models/ideogram.ts
@@ -0,0 +1,63 @@
+import { z } from "zod";
+
+export const IdeogramImageSchema = z.object({
+  is_image_safe: z.boolean(),
+  prompt: z.string(),
+  resolution: z.string(),
+  seed: z.number(),
+  style_type: z.string(),
+  url: z.string(),
+});
+export type IdeogramImage = z.infer<typeof IdeogramImageSchema>;
+
+export const IdeogramResponseSchema = z.object({
+  created: z.coerce.date(),
+  data: z.array(IdeogramImageSchema),
+});
+export type IdeogramResponse = z.infer<typeof IdeogramImageSchema>;
+
+export const AspectRatioList = [
+  "1x3",
+  "3x1",
+  "1x2",
+  "2x1",
+  "9x16",
+  "16x9",
+  "10x16",
+  "16x10",
+  "2x3",
+  "3x2",
+  "3x4",
+  "4x3",
+  "4x5",
+  "5x4",
+  "1x1",
+] as const;
+export type AspectRatio = (typeof AspectRatioList)[number];
+
+export const MagicPromptModeList = ["AUTO", "OFF", "ON"] as const;
+export type MagicPromptMode = (typeof MagicPromptModeList)[number];
+
+export const StyleTypeList = [
+  "AUTO",
+  "GENERAL",
+  "REALISTIC",
+  "DESIGN",
+] as const;
+export type StyleType = (typeof StyleTypeList)[number];
+
+export const IdeogramRequestSchema = z.object({
+  prompt: z.string(),
+  seed: z.string().optional(),
+  resolution: z.string().optional(),
+  aspect_ratio: z.enum(AspectRatioList),
+  magic_prompt: z.enum(MagicPromptModeList).optional(),
+  negative_prompt: z.string().optional(),
+  num_images: z.number().optional(),
+  color_palette: z.object({}).optional(),
+  style_codes: z.array(z.string()).optional(),
+  style_type: z.enum(StyleTypeList).optional(),
+  style_reference_images: z.any().array().optional(),
+});
+
+export type IdeogramRequest = z.infer<typeof IdeogramRequestSchema>;
